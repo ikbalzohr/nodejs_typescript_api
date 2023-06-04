41c7638982ab20c354f6a06809b5b56d3cf5b4ac
ikbalzohr <ikbal.gf12@gmail.com>
2023-06-05 06:06:05 +0800

    inti auth

---
+++ src/models/user_model.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true
+    },
+    password: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    role: {
+      type: String,
+      default: 'regular'
+    }
+  },
+  { timestamps: true }
+)
+
+const userModel = mongoose.model('user', userSchema)
+
+export default userModel

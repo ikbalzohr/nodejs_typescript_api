a7d552ea780c9881e8167979c6db684ecb6c5941
ikbalzohr <ikbal.gf12@gmail.com>
2023-06-05 16:29:25 +0800

    add authorization

--- src/middleware/auth_middleware.ts
+++ src/middleware/auth_middleware.ts
@@ -8,3 +8,12 @@ export const requireUser = (req: Request, res: Response, next: NextFunction): an
 
   next()
 }
+
+export const requireAdmin = (req: Request, res: Response, next: NextFunction): any => {
+  const user = res.locals.user
+  if (!user || user._doc.role !== 'admin') {
+    return res.sendStatus(403)
+  }
+
+  next()
+}
--- src/routes/product_route.ts
+++ src/routes/product_route.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 
 import { createProduct, deleteProduct, getProduct, updateProduct } from '../controller/product_controller'
-import { requireUser } from '../middleware/auth_middleware'
+import { requireAdmin, requireUser } from '../middleware/auth_middleware'
 
 export const ProductRouter: Router = Router()
 
 ProductRouter.get('/', getProduct)
-ProductRouter.get('/:id', getProduct)
-ProductRouter.post('/', requireUser, createProduct)
+ProductRouter.get('/:id', requireUser, getProduct)
+ProductRouter.post('/', requireAdmin, createProduct)
 ProductRouter.put('/:id', updateProduct)
-ProductRouter.delete('/:id', requireUser, deleteProduct)
+ProductRouter.delete('/:id', requireAdmin, deleteProduct)
